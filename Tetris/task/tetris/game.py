dce9fce121c2329611a7cbcc9dc7abb283a54339
Roman92P <roman.paszkow@gmail.com>
2023-03-27 23:38:04 +0200

    commit

--- Tetris/task/tetris/game.py
+++ Tetris/task/tetris/game.py
@@ -1,5 +1,4 @@
 # Write your code here
-import collections
 
 O = [[5, 6, 9, 10]]
 I = [[1, 5, 9, 13], [4, 5, 6, 7]]
@@ -18,13 +17,35 @@ for x in range(0, 4):
 
 input_symbol = str(input())
 
-row_count = 0
-col_count = 0
+
+def printGrid(arr):
+    for row in arr:
+        print(''.join(row))
+    # print('\n')
+
+
+printGrid(grid)
 
 if symbol_arr.__contains__(input_symbol):
     symbol_all_coordinates = globals()[input_symbol]
-    print(symbol_all_coordinates)
-    for row in grid:
-        for col in row:
-            col_count += 1
-    row_count += 1
+
+    counter = 0
+    while counter < len(symbol_all_coordinates) + 1:
+        shape_cords_to_be_printed = ''
+        if counter == len(symbol_all_coordinates):
+            shape_cords_to_be_printed = symbol_all_coordinates[0]
+        else: shape_cords_to_be_printed = symbol_all_coordinates[counter]
+        row_count = 0
+        count = 0
+        for row in grid:
+            col_count = 0
+            for col in row:
+                if count in shape_cords_to_be_printed:
+                    grid[row_count][col_count] = '0 '
+                else:
+                    grid[row_count][col_count] = '- '
+                col_count += 1
+                count += 1
+            row_count += 1
+        printGrid(grid)
+        counter += 1
