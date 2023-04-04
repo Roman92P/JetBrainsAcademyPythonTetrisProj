4646f2d81ccef59a7949aa92a53394db79acf5cf
Roman92P <roman.paszkow@gmail.com>
2023-04-04 21:26:55 +0200

    tasks commit

--- Tetris/task/tetris/game.py
+++ Tetris/task/tetris/game.py
@@ -9,6 +9,114 @@ T = [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]]
 symbol_arr = ['O', 'I', 'S', 'Z', 'L', 'J', 'T']
 
 
+def rotate(current_grid_state):
+    update_shape_cords = []
+    count = 0
+    row_count = 0
+    for i in current_grid_state:
+        col_count = 0
+        for j in i:
+            if j == "0 ":
+                current_grid_state[row_count][col_count] = "- "
+                update_shape_cords.append(count)
+            count += 1
+            col_count += 1
+        row_count += 1
+
+    result = []
+    print(update_shape_cords)
+    if input_symbol == 'I':
+        if update_shape_cords[0] + 3 == update_shape_cords[3]:
+            result.append(update_shape_cords[1])
+            result.append(update_shape_cords[1] + 10)
+            result.append(update_shape_cords[1] + 20)
+            result.append(update_shape_cords[1] + 30)
+        else:
+            result.append(update_shape_cords[0])
+            result.append(update_shape_cords[0] - 1)
+            result.append(update_shape_cords[0] + 1)
+            result.append(update_shape_cords[0] + 2)
+    elif input_symbol == 'S':
+        if update_shape_cords[1] - update_shape_cords[0] == 1 and update_shape_cords[3] - update_shape_cords[2] == 1:
+            result.append(update_shape_cords[0])
+            result.append(update_shape_cords[0] + 10)
+            result.append(update_shape_cords[0] + 10 + 1)
+            result.append(update_shape_cords[0] + 10 + 1 + 10)
+        else:
+            result.append(update_shape_cords[0])
+            result.append(update_shape_cords[0] + 1)
+            result.append(update_shape_cords[2] - 1)
+            result.append(update_shape_cords[2] - 2)
+    elif input_symbol == 'Z':
+        if update_shape_cords[1] - update_shape_cords[0] == 1 and update_shape_cords[3] - update_shape_cords[2] == 1:
+            result.append(update_shape_cords[1])
+            result.append(update_shape_cords[1] + 10)
+            result.append(update_shape_cords[1] + 10 - 1)
+            result.append(update_shape_cords[1] + 10 - 1 + 10)
+        else:
+            result.append(update_shape_cords[0])
+            result.append(update_shape_cords[0] - 1)
+            result.append(update_shape_cords[1] + 1)
+            result.append(update_shape_cords[1] + 2)
+    elif input_symbol == 'L':
+        if update_shape_cords[1] - update_shape_cords[0] == 10 and update_shape_cords[2] - update_shape_cords[1] == 10:
+            result.append(update_shape_cords[0] + 1)
+            result.append(update_shape_cords[0] + 1 + 10)
+            result.append(update_shape_cords[0] + 1 + 10 - 1)
+            result.append(update_shape_cords[0] + 1 + 10 - 2)
+        elif update_shape_cords[2] - update_shape_cords[1] == 1 and update_shape_cords[3] - update_shape_cords[2] == 1:
+            result.append(update_shape_cords[0])
+            result.append(update_shape_cords[0] + 10)
+            result.append(update_shape_cords[0] + 20)
+            result.append(update_shape_cords[0] - 1)
+        elif update_shape_cords[2] - update_shape_cords[1] == 10 and update_shape_cords[3] - update_shape_cords[
+            2] == 10:
+            result.append(update_shape_cords[1] + 1)
+            result.append(update_shape_cords[1])
+            result.append(update_shape_cords[1] - 1)
+            result.append(update_shape_cords[1] - 1 + 10)
+        elif update_shape_cords[1] - update_shape_cords[0] == 1 and update_shape_cords[2] - update_shape_cords[1] == 1:
+            result.append(update_shape_cords[0])
+            result.append(update_shape_cords[0] + 10)
+            result.append(update_shape_cords[0] + 20)
+            result.append(update_shape_cords[0] + 20 + 1)
+    if input_symbol == 'J':
+        if update_shape_cords[1] - update_shape_cords[0] == 10 and update_shape_cords[3] - update_shape_cords[1] == 10:
+            result.append(update_shape_cords[1])
+            result.append(update_shape_cords[1] - 10)
+            result.append(update_shape_cords[1] - 10 - 1)
+            result.append(update_shape_cords[1] - 10 - 2)
+        elif update_shape_cords[1] - update_shape_cords[0] == 1 and update_shape_cords[2] - update_shape_cords[1] == 1:
+            result.append(update_shape_cords[2])
+            result.append(update_shape_cords[2] - 1)
+            result.append(update_shape_cords[2] - 1 + 10)
+            result.append(update_shape_cords[2] - 1 + 20)
+        elif update_shape_cords[2] - update_shape_cords[0] == 10 and update_shape_cords[3] - update_shape_cords[2] == 10:
+            result.append(update_shape_cords[0])
+            result.append(update_shape_cords[0] + 10)
+            result.append(update_shape_cords[0] + 10 + 1)
+            result.append(update_shape_cords[0] + 10 + 2)
+        elif update_shape_cords[2] - update_shape_cords[1] == 1 and update_shape_cords[3] - update_shape_cords[2] == 1:
+            result.append(update_shape_cords[0] + 1)
+            result.append(update_shape_cords[0] + 1 + 10)
+            result.append(update_shape_cords[0] + 1 + 20)
+            result.append(update_shape_cords[0] + 20)
+    if input_symbol != 'T':
+
+    print(result)
+    temp_count = 0
+    row_count = 0
+    for k in current_grid_state:
+        col_count = 0
+        for m in k:
+            if result.__contains__(temp_count):
+                current_grid_state[row_count][col_count] = '0 '
+            temp_count += 1
+            col_count += 1
+        row_count += 1
+    return current_grid_state
+
+
 def create_grid(grid_wh):
     grid = []
     temp_wh_arr = grid_wh.split(' ')
@@ -56,29 +164,72 @@ def fill_the_grid_with_starting_pos(two_dm_arr, input_shape):
 
 
 def move_down(current_grid_state):
-    current_shape_cords = []
-    grid_width = len(current_grid_state)
+    update_shape_cords = []
     count = 0
-    for x in current_grid_state:
-        if x != '- ':
-            current_shape_cords.append(count + grid_width)
-            x = '- '
-        count += 1
+    row_count = 0
+    for i in current_grid_state:
+        col_count = 0
+        for j in i:
+            if j == "0 ":
+                current_grid_state[row_count][col_count] = "- "
+                update_shape_cords.append(str(int(row_count + 1)) + " " + str(col_count))
+            count += 1
+            col_count += 1
+        row_count += 1
+    for k in update_shape_cords:
+        new_coordinates = k.split(" ")
+        current_grid_state[int(new_coordinates[0])][int(new_coordinates[1])] = '0 '
+    return current_grid_state
+
+
+def move_right(current_grid_state):
+    update_shape_cords = []
     count = 0
-    for k in current_grid_state:
-        if current_shape_cords.__contains__(count):
-            k = '0 '
+    row_count = 0
+    for i in current_grid_state:
+        col_count = 0
+        for j in i:
+            if j == "0 ":
+                current_grid_state[row_count][col_count] = "- "
+                update_shape_cords.append(str(row_count) + " " + str(int(col_count + 1)))
+            count += 1
+            col_count += 1
+        row_count += 1
+    for k in update_shape_cords:
+        new_coordinates = k.split(" ")
+        current_grid_state[int(new_coordinates[0])][int(new_coordinates[1])] = '0 '
+    return current_grid_state
+
+
+def move_left(current_grid_state):
+    update_shape_cords = []
+    count = 0
+    row_count = 0
+    for i in current_grid_state:
+        col_count = 0
+        for j in i:
+            if j == "0 ":
+                current_grid_state[row_count][col_count] = "- "
+                update_shape_cords.append(str(row_count) + " " + str(int(col_count - 1)))
+            count += 1
+            col_count += 1
+        row_count += 1
+    for k in update_shape_cords:
+        new_coordinates = k.split(" ")
+        current_grid_state[int(new_coordinates[0])][int(new_coordinates[1])] = '0 '
     return current_grid_state
 
 
 def move_shape(user_command, current_grid_state):
-    move_down(current_grid_state)
+    # move_down(current_grid_state)
     if user_command == 'down':
         move_down(current_grid_state)
     elif user_command == 'right':
-        move_down(current_grid_state)
+        move_right(current_grid_state)
     elif user_command == 'left':
-        move_down(current_grid_state)
+        move_left(current_grid_state)
+    elif user_command == 'rotate':
+        rotate(current_grid_state)
     return current_grid_state
 
 
